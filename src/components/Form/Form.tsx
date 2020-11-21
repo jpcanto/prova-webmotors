4c9bf10ec3ffb5d4a6aae47caf9d42c2c2f2a22a
jpcanto <jplopessk8@gmail.com>
2020-11-21 15:34:17 -0300

    feat: adding year and value inputs

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -3,14 +3,18 @@ import React from "react";
 import { FormComponent } from "./Form.styled";
 import CheckBox from "../../components/CheckBox/CheckBox";
 import Select from "../../components/Select/Select";
+import Input from "../../components/Input/Input";
 
 const Form: React.FC = () => {
   return (
     <FormComponent>
       <CheckBox label="Novos" />
       <CheckBox label="Usados" />
+
       <Select mode="maker" />
       <Select mode="model" />
+      <Input type="number" min="2012" max="2018" placeholder="Ano Desejado" />
+      <Input type="number" min="0" placeholder="Faixa de Preço" />
       <Select mode="version" />
     </FormComponent>
   );
