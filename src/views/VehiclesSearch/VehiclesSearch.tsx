ff64006e5f5b45e0b4c40465eca1ae6344194a23
jpcanto <jplopessk8@gmail.com>
2020-11-21 10:34:07 -0300

    feat: consuming vehicles on V-search view

--- src/views/VehiclesSearch/VehiclesSearch.tsx
+++ src/views/VehiclesSearch/VehiclesSearch.tsx
@@ -5,6 +5,7 @@ import { StoreState } from "../../store/createStore";
 import { vehiclesMakersRequest } from "../../store/modules/vehiclesMakers/actions";
 import { vehiclesModelsRequest } from "../../store/modules/vehiclesModels/actions";
 import { vehiclesVersionsRequest } from "../../store/modules/vehiclesVersions/actions";
+import { vehiclesRequest } from "../../store/modules/vehicles/actions";
 
 import PreLoader from "../../components/PreLoader/PreLoader";
 import Select from "../../components/Select/Select";
@@ -13,20 +14,23 @@ const VehiclesSearch: React.FC = () => {
   const VMakers = useSelector((state: StoreState) => state.vehiclesMakers);
   const VModels = useSelector((state: StoreState) => state.vehiclesModels);
   const VVersions = useSelector((state: StoreState) => state.vehiclesVersions);
+  const Vehicles = useSelector((state: StoreState) => state.vehicles);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(vehiclesMakersRequest());
     dispatch(vehiclesModelsRequest({ MakeID: 1 }));
     dispatch(vehiclesVersionsRequest({ ModelID: 1 }));
+    dispatch(vehiclesRequest({ Page: 1 }));
   }, [dispatch]);
 
   return (
     <>
-      {VMakers.loading || VModels.loading ? <PreLoader /> : null}
+      {VMakers.loading || VModels.loading || VVersions.loading ? <PreLoader /> : null}
       {VMakers.vehiclesMakers ? <Select mode="maker" /> : null}
       {VModels.vehiclesModels ? <Select mode="models" /> : null}
       {VVersions.vehiclesVersions ? <Select mode="versions" /> : null}
+      {Vehicles.vehicles ? <Select mode="versions" /> : null}
     </>
   );
 };
