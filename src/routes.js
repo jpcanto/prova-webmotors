72ce954497741a17b715bfebc52924926c8b0164
jpcanto <jplopessk8@gmail.com>
2020-11-20 21:19:31 -0300

    feat: adding router config

---
+++ src/routes.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Router, Switch, Route, Redirect, Link } from 'react-router-dom';
+
+import App from './App';
+import NotFound from './views/NotFound/NotFound';
+
+import { createBrowserHistory } from 'history';
+
+const history = createBrowserHistory();
+
+const Routes = () => {
+
+  return <Router history={history}>
+    <nav>
+      <Link to="/" />
+    </nav>
+    <Switch>
+      <Route exact path='/' render={() => <App />} />
+      <Route path='/404' render={() => <NotFound />} />
+      <Redirect to="/404" />
+    </Switch>
+  </Router>
+
+}
+
+export default Routes;
