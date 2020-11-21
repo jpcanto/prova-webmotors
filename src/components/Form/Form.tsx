a820b19016e2b3427182237c66d4593be3dee396
jpcanto <jplopessk8@gmail.com>
2020-11-21 15:47:44 -0300

    feat: adding label button component

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -4,6 +4,7 @@ import { FormComponent } from "./Form.styled";
 import CheckBox from "../../components/CheckBox/CheckBox";
 import Select from "../../components/Select/Select";
 import Input from "../../components/Input/Input";
+import LabelButton from "../../components/LabelButton/LabelButton";
 
 const Form: React.FC = () => {
   return (
@@ -16,6 +17,8 @@ const Form: React.FC = () => {
       <Input type="number" min="2012" max="2018" placeholder="Ano Desejado" />
       <Input type="number" min="0" placeholder="Faixa de Preço" />
       <Select mode="version" />
+      <LabelButton color="#89414b" fontSize=".9em" text="Busca Avançada" bold={true} />
+      <LabelButton color="#a8a8a8" fontSize=".9em" text="Limpar Filtros" bold={false} />
     </FormComponent>
   );
 };
