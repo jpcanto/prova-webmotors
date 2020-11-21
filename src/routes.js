eb5337fd9147366cb0842d0f5fd9ba09d8e36d5f
jpcanto <jplopessk8@gmail.com>
2020-11-20 21:25:47 -0300

    fix: cleaning unused Link import

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Router, Switch, Route, Redirect, Link } from 'react-router-dom';
+import { Router, Switch, Route, Redirect } from 'react-router-dom';
 
 import App from './App';
 import NotFound from './views/NotFound/NotFound';
