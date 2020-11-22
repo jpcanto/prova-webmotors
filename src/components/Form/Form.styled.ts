970a0fe5330a9a4e893c27775a2e6448777ab71f
jpcanto <jplopessk8@gmail.com>
2020-11-22 15:07:09 -0300

    style: adding some responsive rules

--- src/components/Form/Form.styled.ts
+++ src/components/Form/Form.styled.ts
@@ -4,6 +4,10 @@ export const FormComponent = styled.form`
   background-color: #fff;
   max-height: 312px;
 
+  @media(max-width: 760px) {
+    max-height: 100%;
+  }
+
   [direction="column"] {
 
     > [direction="column"] {
@@ -20,22 +24,44 @@ export const FormComponent = styled.form`
         margin-right: 10px;
       }
 
+      @media(max-width: 760px) {
+        > [direction="row"] > select, > [direction="row"] > input {
+          width: 40vw !important;
+          margin: 10px 10px 0 0 !important;
+        }
+      }
+
     }
 
     > [direction="row"] {
 
       margin: 15px 0;
 
+      @media(max-width: 760px) {
+        flex-wrap: wrap;
+      }
+
       label:not(:first-of-type) {
         margin-left: 40px;
       }
 
-      /* select:not(:first-child), input {
-        margin-left: 10px;
-      } */
+      & > [direction="row"] {
 
-      & > [direction="row"] label {
-        width: 13vw;
+        label {
+          width: 13vw;
+        }
+
+        @media(max-width: 760px) {
+          margin-top: 15px;
+
+          label {
+            width: 45vw !important;
+          }
+
+          button {
+            font-size: .9em;
+          }
+        }
       }
     }
   }
