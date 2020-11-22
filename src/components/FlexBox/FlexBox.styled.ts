2430f14a6e6d99b26584c98bf70d27720d989549
jpcanto <jplopessk8@gmail.com>
2020-11-22 01:39:09 -0300

    style: adding active effect in header buttons

--- src/components/FlexBox/FlexBox.styled.ts
+++ src/components/FlexBox/FlexBox.styled.ts
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 interface IFlex {
+  active?: boolean;
   align?: string;
   direction: string;
   bgcolor?: string;
@@ -11,6 +12,7 @@ interface IFlex {
 export const FlexBox = styled.div<IFlex>`
   align-items: ${props => props.align ? props.align : 'center'};
   background-color: ${props => props.bgcolor};
+  border-bottom: ${props => props.active ? '3px solid #ca2430' : null};
   display: flex;
   flex-direction: ${props => props.direction};
   justify-content: ${props => props.position};
--- src/components/Form/FormHeader/FormHeader.tsx
+++ src/components/Form/FormHeader/FormHeader.tsx
@@ -1,28 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { Header } from "./FormHeader.styled";
 import { FlexBox } from "../../FlexBox/FlexBox.styled";
 
 import Button from "../../Button/Button";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 import Logo from "../../../assets/img/logo.png";
 import Car from "../../../assets/img/car.png";
 import Bike from "../../../assets/img/bike.png";
 
 const FormHeader: React.FC = () => {
+  const [active, setActive] = useState<string>("car");
+  const errorNotify = () =>
+    toast.error(
+      "⚠️ Oops, essa funcionalidade ainda não foi implementada... Atualmente só temos filtros para carros",
+      {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      }
+    );
+
+  function handleClick(type: string) {
+    setActive(type);
+    errorNotify();
+  }
+
   return (
     <Header>
       <FlexBox direction="column" align="flex-start" position="space-around">
         <img src={Logo} />
         <FlexBox direction="row">
-          <FlexBox direction="row">
+          <FlexBox
+            direction="row"
+            active={active === "car" ? true : false}
+            onClick={() => handleClick("car")}
+          >
             <img src={Car} />
             <FlexBox direction="column">
               <p>COMPRAR</p>
               <p>CARROS</p>
             </FlexBox>
           </FlexBox>
-          <FlexBox direction="row">
+          <FlexBox
+            direction="row"
+            active={active === "bike" ? true : false}
+            onClick={() => handleClick("bike")}
+          >
             <img src={Bike} />
             <FlexBox direction="column">
               <p>COMPRAR</p>
