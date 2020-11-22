cb6594fcfeed783283cf4cebf6cdaac4813570ef
jpcanto <jplopessk8@gmail.com>
2020-11-22 15:33:53 -0300

    style: adding some responsive rules

--- src/components/Form/FormHeader/FormHeader.styled.ts
+++ src/components/Form/FormHeader/FormHeader.styled.ts
@@ -11,15 +11,42 @@ export const Header = styled.div`
       width: 20%;
     }
 
+    > img {
+      @media(max-width: 760px) {
+        width: 40%;
+      }
+    }
+
     [direction="row"] {
       :first-of-type {
         margin-top: auto;
+
+        @media(max-width: 760px) {
+          flex-wrap: wrap;
+        }
       }
 
       [direction="row"] {
         cursor: pointer;
         padding-bottom: 8px;
         width: 15%;
+
+        @media(max-width: 760px) {
+          height: 7vh;
+          flex-wrap: nowrap;
+          justify-content: center;
+          padding: 0;
+          width: 50vw;
+
+          &:not(:first-child) {
+            margin-left: 0 !important;
+          }
+
+          div {
+            align-items: center;
+            justify-content: center;
+          }
+        }
 
         &:not(:first-child) {
           margin-left: 20px;
@@ -46,6 +73,13 @@ export const Header = styled.div`
         height: 4vh;
         margin-left: auto;
         margin-bottom: 8px;
+
+        @media(max-width: 760px) {
+          font-size: .7em;
+
+          height: 3vh;
+          width: 50vw;
+        }
       }
 
     }
