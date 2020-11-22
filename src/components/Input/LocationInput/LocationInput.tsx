0ad475aeec50726d7c4082fca65a9d09c50da2d4
jpcanto <jplopessk8@gmail.com>
2020-11-22 00:21:17 -0300

    feat: setting an alternative text to img

--- src/components/Input/LocationInput/LocationInput.tsx
+++ src/components/Input/LocationInput/LocationInput.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 
 import Input from "../Input";
-import locationIcon from '../../../assets/img/location.png';
+import locationIcon from "../../../assets/img/location.png";
 import { Combo } from "./LocationInput.styled";
 
 const LocationInput: React.FC = () => {
   return (
     <Combo className="location">
-      <img src={locationIcon}></img>
-      <Input type="text" placeholder="Onde: São Paulo - SP"/>
+      <img src={locationIcon} alt="location icon"></img>
+      <Input type="text" placeholder="Onde: São Paulo - SP" />
       <Input type="number" min="0" max="300" placeholder="Raio: 100km" />
     </Combo>
   );
