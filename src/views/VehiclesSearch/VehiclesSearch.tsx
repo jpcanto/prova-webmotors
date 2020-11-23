efd37b934d4c0d0645da5c0f07d878380f1e5eff
jpcanto <jplopessk8@gmail.com>
2020-11-23 09:37:27 -0300

    feat: cleaning unused imports and store calls

--- src/views/VehiclesSearch/VehiclesSearch.tsx
+++ src/views/VehiclesSearch/VehiclesSearch.tsx
@@ -1,17 +1,10 @@
 import React from "react";
-// import { useSelector } from "react-redux";
-
-// import { StoreState } from "../../store/createStore";
 
 import Form from "../../components/Form/Form";
 import FilteredVehicles from "../../components/FilteredVehicles/FilteredVehicles";
 import { Container } from "./VehiclesSearch.styled";
 
 const VehiclesSearch: React.FC = () => {
-  // const VMakers = useSelector((state: StoreState) => state.vehiclesMakers);
-  // const VModels = useSelector((state: StoreState) => state.vehiclesModels);
-  // const VVersions = useSelector((state: StoreState) => state.vehiclesVersions);
-  // const Vehicles = useSelector((state: StoreState) => state.vehicles);
 
   return (
     <Container>
